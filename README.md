37e7a3af1eaaacc15db69269e137271296577dde
Sebastian Cabezas <scabezas@ciisa.cl>
2024-04-13 09:54:06 -0400

    links onedrive

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # backend-sec70-2024t1
+Acceso a OneDrive con Video de Clases
+
+BACKEND - 70
+https://ipciisa-my.sharepoint.com/:f:/g/personal/scabezas_ciisa_cl/EooRFIEyrOlEj5JwnSiqOsUBI7DxFCfwdk8HjgQE8JC4vw?e=nPemlQ
+
+FRONTEND - 70
+https://ipciisa-my.sharepoint.com/:f:/g/personal/scabezas_ciisa_cl/EtYrGrCCBMlDvOJQmmwIywABFgy6bri6eB86QZheOpxyDg?e=DX4LIR
--- v1/mantenedor/index.php
+++ v1/mantenedor/index.php
@@ -19,12 +19,34 @@ if($_version == 'v1'){
         switch ($_metodo){
             case 'GET':
                 if($_header == $_token_get){
-                    include_once 'controller.php';
+                    /*include_once 'controller.php';
                     include_once '../conexion.php';
                     $control = new Controlador();
                     $lista = $control->getAll();
                     http_response_code(200);
                     echo json_encode(['data' => $lista]);
+                    */
+                    
+                    $retorno = [
+                        [
+                            "id" => 0,
+                            "nombre" => 'Wena1',
+                            "activo" => true
+                        ],
+                        [
+                            "id" => 1,
+                            "nombre" => 'Wena2',
+                            "activo" => false
+                        ],
+                        [
+                            "id" => 2,
+                            "nombre" => 'Wena3',
+                            "activo" => true
+                        ],
+                    ];
+                    http_response_code(200);
+                    echo json_encode(['data' => $retorno]);
+
                 }else{
                     http_response_code(401);
                     echo json_encode(['error' => 'no tiene autorizacion get']);

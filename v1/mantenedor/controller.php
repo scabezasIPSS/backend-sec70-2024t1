26b574b2dcbc83ecd8eee8dd9c4b889fc31069de
scabezasciisa <scabezas@ciisa.cl>
2024-05-10 20:50:11 -0400

    dev: antes prueba 2

--- v1/mantenedor/controller.php
+++ v1/mantenedor/controller.php
@@ -1,6 +1,7 @@
 <?php
 
-class Controlador{
+class Controlador
+{
 
     private $lista;
 
@@ -9,13 +10,14 @@ class Controlador{
         $this->lista = [];
     }
 
-    public function getAll(){
+    public function getAll()
+    {
         $con = new Conexion();
         $sql = "SELECT id, nombre, activo FROM mantenedor;";
         $rs = mysqli_query($con->getConnection(), $sql);
-        if ($rs){
-            while ($tupla = mysqli_fetch_assoc($rs)){
-                $tupla['activo'] = $tupla['activo'] == 1 ? true: false;
+        if ($rs) {
+            while ($tupla = mysqli_fetch_assoc($rs)) {
+                $tupla['activo'] = $tupla['activo'] == 1 ? true : false;
                 array_push($this->lista, $tupla);
             }
             mysqli_free_result($rs);
@@ -24,4 +26,86 @@ class Controlador{
         return $this->lista;
     }
 
+    public function postNuevo($_newObject)
+    {
+        $con = new Conexion();
+        $id = count($this->getAll()) + 1;
+        // echo 'el nuevo id: ' . $id;
+        // var_dump($_newObject->nombre);
+        $sql = "INSERT INTO mantenedor (id, nombre, activo) VALUES ($id,'$_newObject->nombre',true);";
+        // echo $sql;
+        $rs = false;
+        try {
+            $rs = mysqli_query($con->getConnection(), $sql);
+        } catch (\Throwable $th) {
+            $rs = false;
+        }
+        // cerramos la conexion
+        $con->closeConnection();
+        // comprobamos la respuesta
+        if ($rs) {
+            return true;
+        }
+        return null;
+    }
+
+    public function patchEncenderApagar($_id, $_accion)
+    {
+        $con = new Conexion();
+        $sql = "UPDATE mantenedor SET activo = $_accion WHERE id = $_id;";
+        // echo $sql;
+        $rs = false;
+        try {
+            $rs = mysqli_query($con->getConnection(), $sql);
+        } catch (\Throwable $th) {
+            $rs = false;
+        }
+        // cerramos la conexion
+        $con->closeConnection();
+        // comprobamos la respuesta
+        if ($rs) {
+            return true;
+        }
+        return null;
+    }
+
+    public function putNombreById($_nombre, $_id)
+    {
+        $con = new Conexion();
+        $sql = "UPDATE mantenedor SET nombre = '$_nombre' WHERE id = $_id;";
+        // echo $sql;
+        $rs = false;
+        try {
+            $rs = mysqli_query($con->getConnection(), $sql);
+        } catch (\Throwable $th) {
+            $rs = false;
+        }
+        // cerramos la conexion
+        $con->closeConnection();
+        // comprobamos la respuesta
+        if ($rs) {
+            return true;
+        }
+        return null;
+    }
+
+    public function deleteById($_id)
+    {
+        $con = new Conexion();
+        $sql = "DELETE FROM mantenedor WHERE id = $_id;";
+        // echo $sql;
+        $rs = false;
+        try {
+            $rs = mysqli_query($con->getConnection(), $sql);
+        } catch (\Throwable $th) {
+            $rs = false;
+        }
+        // cerramos la conexion
+        $con->closeConnection();
+        // comprobamos la respuesta
+        if ($rs) {
+            return true;
+        }
+        return null;
+    }
 }
--- v1/mantenedor/index.php
+++ v1/mantenedor/index.php
@@ -4,31 +4,111 @@ include_once '../version1.php';
 //parametros
 $existeId = false;
 $valorId = 0;
+$existeAccion = false;
+$valorAccion = 0;
 
 
-if (count($_parametros)>0){
-    foreach($_parametros as $p){
-        if(strpos($p, 'id') !== false){
+if (count($_parametros) > 0) {
+    foreach ($_parametros as $p) {
+        if (strpos($p, 'id') !== false) {
             $existeId = true;
             $valorId = explode('=', $p)[1];
+        }
+        if (strpos($p, 'accion') !== false) {
+            $existeAccion = true;
+            $valorAccion = explode('=', $p)[1];
         }
     }
 }
 
-if($_version == 'v1'){
-    if ($_mantenedor == 'mantenedor'){
-        switch ($_metodo){
+if ($_version == 'v1') {
+    if ($_mantenedor == 'mantenedor') {
+        switch ($_metodo) {
             case 'GET':
-                if($_header == $_token_get){
+                if ($_header == $_token_get) {
                     include_once 'controller.php';
                     include_once '../conexion.php';
                     $control = new Controlador();
                     $lista = $control->getAll();
                     http_response_code(200);
                     echo json_encode(['data' => $lista]);
-                }else{
+                } else {
                     http_response_code(401);
                     echo json_encode(['error' => 'no tiene autorizacion get']);
+                }
+                break;
+            case 'POST':
+                if ($_header == $_token_post) {
+                    include_once 'controller.php';
+                    include_once '../conexion.php';
+                    $control = new Controlador();
+                    $body = json_decode(file_get_contents("php://input", true));
+                    //trae el getall y corrobora q el dato a insertar no exista. Si existe. salta al error 409. 
+                    //Si no existe, entonces se intenta insertar
+                    $respuesta = $control->postNuevo($body);
+                    if ($respuesta) {
+                        http_response_code(201);
+                        echo json_encode(['data' => $respuesta]);
+                    } else {
+                        http_response_code(409);
+                        echo json_encode(['error' => 'tiene conflicto pq el dato ya existe']);
+                    }
+                } else {
+                    http_response_code(401);
+                    echo json_encode(['error' => 'no tiene autorizacion post']);
+                }
+                break;
+            case 'PATCH':
+                if ($_header == $_token_patch) {
+                    include_once 'controller.php';
+                    include_once '../conexion.php';
+                    $control = new Controlador();
+                    if ($existeId && $existeAccion) {
+                        if ($valorAccion == 'apagar') {
+                            $respuesta = $control->patchEncenderApagar($valorId, 'false');
+                            // echo "patch... $valorId - $valorAccion";
+                            http_response_code(200);
+                            echo json_encode(['data' => $respuesta]);
+                        } else if ($valorAccion == 'encender') {
+                            $respuesta = $control->patchEncenderApagar($valorId, 'true');
+                            http_response_code(200);
+                            echo json_encode(['data' => $respuesta]);
+                        } else {
+                            echo 'error con acciones';
+                        }
+                    } else {
+                        echo 'faltan parametros';
+                    }
+                } else {
+                    http_response_code(401);
+                    echo json_encode(['error' => 'no tiene autorizacion patch']);
+                }
+                break;
+            case 'PUT':
+                if ($_header == $_token_put) {
+                    include_once 'controller.php';
+                    include_once '../conexion.php';
+                    $body = json_decode(file_get_contents("php://input", true));
+                    $control = new Controlador();
+                    $respuesta = $control->putNombreById($body->nombre, $body->id);
+                    http_response_code(200);
+                    echo json_encode(['data' => $respuesta]);
+                } else {
+                    http_response_code(401);
+                    echo json_encode(['error' => 'no tiene autorizacion put']);
+                }
+                break;
+            case 'DELETE':
+                if ($_header == $_token_delete) {
+                    include_once 'controller.php';
+                    include_once '../conexion.php';
+                    $control = new Controlador();
+                    $respuesta = $control->deleteById($valorId);
+                    http_response_code(200);
+                    echo json_encode(['data' => $respuesta]);
+                } else {
+                    http_response_code(401);
+                    echo json_encode(['error' => 'no tiene autorizacion put']);
                 }
                 break;
             default:
